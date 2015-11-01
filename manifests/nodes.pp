dd29e2f234a6f85aa18da924e4be0285c76f3b82
bodik24 <forbodya@gmail.com>
2015-11-01 17:47:18 -0500

    added security rulles + ssh configs

--- manifests/nodes.pp
+++ manifests/nodes.pp
@@ -1,5 +1,24 @@
 node 'demo' {
+  
+  include ssh
+  include sudoers
   include nginx
+ 
+ 
+  user { 'art':
+    ensure  => present,
+    comment => 'Art U',
+    home    => '/home/art',
+    managehome => true
+  }
+
+  ssh_authorized_key { 'art_ssh':
+    user => 'art',
+    type => 'rsa',
+    key => '',
+  }
+
+  
 
   package { 'puppet':
     ensure => latest,
